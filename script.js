1742ad6ac159ec68477a9e9b958881f591173594
Maricarmen <nemracira1718@gmail.com>
2023-02-05 14:08:42 -0500

    feat:Creando Header

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let buttonEdit = document.querySelector(".profile__edit");
+let buttonClose = document.querySelector(".popup__close-icon");
+let formElement = document.querySelector(".popup");
+
+let inputName = document.querySelector("#name");
+let inputAbout = document.querySelector("#about");
+let profileName = document.querySelector(".profile__name");
+let profileOcupation = document.querySelector(".profile__ocupation");
+let buttonSubmit = document.querySelector(".popup__button");
+
+function openPopup () {
+    formElement.classList.add("popup");
+}
+function closePopup () {
+    formElement.classList.remove()
+}
+buttonEdit.addEventListener ("click",openPopup);
+buttonClose.addEventListener ("click",closePopup);
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileOcupation.textContent = inputAbout.value;
+}
+buttonSubmit.addEventListener("click",handleProfileFormSubmit);*/
