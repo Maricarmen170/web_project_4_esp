d0041174acaf0b30a01ad170b6846b34ec733952
Maricarmen <nemracira1718@gmail.com>
2023-03-17 21:27:11 -0500

    feat:mensaje de error en los inputs

---
+++ formValidator.js
@@ -0,0 +1,78 @@
+export class FormValidator {
+    constructor(setting, formElement) {
+        this._inputSelector = setting.inputSelector;
+        this._submitButtonSelector = setting.submitButtonSelector;
+        this._inactiveButtonClass = setting.inactiveButtonClass;
+        this._inputErrorClass = setting.inputErrorClass;
+        this._errorClass = setting.errorClass;
+        this._formElement = formElement;
+    }
+
+    enableValidation() {
+    const inputList = Array.from(
+        this._formElement.querySelectorAll(this._inputSelector)
+    );
+    const buttonElement = this._formElement.querySelector(
+        this._submitButtonSelector
+    );
+
+    this._toggleButtonState(inputList, buttonElement);
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener("input", () => {
+            this._isValid(this._formElement, inputElement);
+            this._toggleButtonState(inputList, buttonElement);
+        });
+    });
+    }
+
+    /** Lógica del formulario*/
+    _isValid(formElement, inputElement) {
+        inputElement.validity.valid
+        ? this._hideInputError(formElement, inputElement)
+        : this._showInputError(
+            formElement,
+            inputElement,
+            inputElement.validationMessage
+        );
+    }
+
+    _showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+    }
+
+    _hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+
+    inputElement.classList.remove(this._inputErrorClass);
+    
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+    }
+
+    /** Lógica del botón */ 
+    _toggleButtonState(inputList, buttonElement) {
+    if (this._hasInvalidInput(inputList)) {
+        this._disabledButton(buttonElement);
+    } else {
+        this._enableButton(buttonElement);
+    }
+    }
+
+    _hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => !inputElement.validity.valid);
+    }
+
+    _disabledButton(buttonElement) {
+    buttonElement.classList.add(this._inactiveButtonClass);
+    buttonElement.setAttribute("disabled", true);
+    }
+
+    _enableButton(buttonElement) {
+    buttonElement.classList.remove(this._inactiveButtonClass);
+    buttonElement.removeAttribute("disabled", false);
+    }
+};
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 import { initialCards } from "./constants.js"
+import {FormValidator} from "./formValidator.js"
 const buttonEdit = document.querySelector('.profile__edit')
-const formElement = document.querySelector('.popup')
+const formElement = document.querySelector('#popup-profile')
 const buttonClose = document.querySelector('#close-icon-popup')
-const buttonSubmit = document.querySelector('.popup__button')
+const buttonSubmit = document.querySelector('#submit-profile')
 const inputName = document.querySelector('#name')
 const inputAbout = document.querySelector('#about')
 const profileName = document.querySelector('.profile__name')
 const profileOcupation = document.querySelector('.profile__ocupation')
 const buttonAdd = document.querySelector('.profile__add-button')
-const formAdd = document.querySelector('.profile__popup-add')
+const formAdd = document.querySelector('#popup-place')
 const buttonX = document.querySelector('#close-icon-cards')
 const inputTitle = document.querySelector('#title')
-const inputSrc = document.querySelector('#src')
+const inputSrc = document.querySelector('#link')
+const buttonCreateCard = document.querySelector('#submit-place')
 
 function openPopup(){
   formElement.classList.add('popup__opened')
@@ -39,11 +41,11 @@ formElement.addEventListener('submit',handleProfileFormSubmit)
 
 
 function openPopupAdd(){
-  formAdd.classList.add('profile__popup-opened')
+  formAdd.classList.add('popup__opened')
   document.addEventListener("keyup", handleEscUp);
 }
 function closePopupAdd(){
-  formAdd.classList.remove('profile__popup-opened')
+  formAdd.classList.remove('popup__opened')
   document.removeEventListener("keyup", handleEscUp);
 }
 buttonAdd.addEventListener('click',openPopupAdd);
@@ -51,7 +53,7 @@ buttonX.addEventListener('click',closePopupAdd);
 
 //renderizar las primeras tarjetas
 const elementsGrid = document.querySelector('.elements');
-const cardTemplateContent = document.querySelector ('#elements-template').content;
+const cardTemplateContent = document.querySelector ('#elements-template').content; 
 
 //function expandElement (
   const modalPopupCard = document.querySelector('.modal__popup-img')
@@ -72,12 +74,13 @@ const cardTemplateContent = document.querySelector ('#elements-template').conten
   function openModalTitle(){
     modalPopupTitle.classList.add('modal__popup-img-title-opened')
   }
-
+// Funcion para agregar nuevas tarjetas
 function createCardElement (link, name) {
   const cardElement = cardTemplateContent.cloneNode(true)
   const element = cardElement.querySelector('.element')
   const elementImage = cardElement.querySelector('.element__image')
   const elementText = cardElement.querySelector('.element__footer-text')
+  const likeBtn = cardElement.querySelector(".icon-like");
   const trashCan = cardElement.querySelector('.icon-remove')
   elementImage.src = link;
   elementImage.alt = name;
@@ -89,6 +92,9 @@ function createCardElement (link, name) {
     openModalCard()
     modalPopupImage.src = link;
   })
+  likeBtn.addEventListener("click", function () {
+    likeBtn.classList.toggle("icon-like_active");
+  });
   elementImage.addEventListener('click',function(){
     openModalTitle()
     modalPopupTitle.alt = name;
@@ -99,7 +105,6 @@ function createCardElement (link, name) {
 
 function renderInitialCards () {
   for (let card of initialCards) {
-    console.log(card)
     const cardElement = createCardElement(card.src, card.name)
     elementsGrid.append(cardElement);
   }
@@ -109,8 +114,10 @@ function addNewCard(evt) {
 evt.preventDefault()
 const cardElement = createCardElement(inputSrc.value,inputTitle.value)
 elementsGrid.prepend(cardElement);
-togglePopup()
+openPopupAdd();
+closePopupAdd();
 }
+buttonCreateCard.addEventListener('click',addNewCard)
 formAdd.addEventListener('submit',addNewCard)
 
 //Ejecucion de codigo al presionar una tecla
@@ -141,6 +148,28 @@ const closeModalOnClick = (evt) => {
 formAdd.addEventListener('click',closeModalOnClick)
 formElement.addEventListener('click',closeModalOnClick)
 modalPopupCard.addEventListener('click',closeModalOnClick)
+
+//mensaje de error a los inputs
+const settingElement = {
+  inputSelector: ".popup__text",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_inactive",
+  inputErrorClass: "popup__text_type_error",
+  errorClass: "popup__input-error_active",
+};
+
+(function init() {
+  const formList = Array.from(
+    document.querySelectorAll(".popup__container")
+  );
+
+  formList.forEach((formElement) => {
+    const validatorForm = new FormValidator(settingElement, formElement);
+    validatorForm.enableValidation();
+  });
+})();
+
+
 
 
 
