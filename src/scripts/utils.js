9918fc279692c2cf8651721f9a1ef0cab8f3bbcd
Maricarmen <nemracira1718@gmail.com>
2023-08-19 18:36:36 -0500

    correccion

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,7 +3,7 @@ export const buttonEdit = document.querySelector('.profile__edit')
 export const profileName = document.querySelector('.profile__name')
 export const profileOcupation = document.querySelector('.profile__ocupation')
 export const profileAvatar = document.querySelector('.profile__image-avatar')
-export const buttonEditAvatar = document.querySelector('.profile__image-avatar_button')
+export const buttonEditAvatar = document.querySelector('.profile__image-button')
 export const buttonDelete = document.querySelector('#popup__trash_button')
 
 
